f9d411b0b6c29b3241843234e95917f017c5e2e0
Abrar Hasan <abrarhasan@Abrars-MacBook-Pro.local>
2022-04-10 14:25:27 +0530

    Add. searching for options

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const addToSelectedOptions = (option) => {
   selectedOptionsElement.appendChild(p);
 };
 
+const addNoOptionsWhenNoOptionsAreAvailable = () => {
+  if (optionsElement.children.length == 0) {
+    optionsElement.innerHTML = `<p style='text-align:center'>No Options Available..</p>`;
+  }
+};
+
 const addOptionsToDropDown = (options) => {
   const optionNodes = options.map((option) => {
     const p = document.createElement("p");
@@ -27,28 +33,59 @@ const addOptionsToDropDown = (options) => {
     p.addEventListener("click", () => {
       addToSelectedOptions(option);
       optionsElement.removeChild(p);
+      addNoOptionsWhenNoOptionsAreAvailable();
       chosenOptions.push(option);
     });
     return p;
   });
   optionsElement.innerHTML = "";
   optionNodes.forEach((o) => optionsElement.appendChild(o));
+  addNoOptionsWhenNoOptionsAreAvailable();
 };
 
 addOptionsToDropDown(gameOptions);
 
 arrowElement.addEventListener("click", (e) => {
   if (optionsElement.classList.contains("closed")) {
-    arrowElement.setAttribute(
-      "src",
-      "https://img.icons8.com/ios/20/000000/delete-sign--v1.png"
+    showOptions();
+    const filteredOptions = gameOptions.filter(
+      (option) => !chosenOptions.includes(option)
     );
-    optionsElement.classList.remove("closed");
+    addOptionsToDropDown(filteredOptions);
   } else {
-    arrowElement.setAttribute(
-      "src",
-      "https://img.icons8.com/ios/20/000000/expand-arrow--v2.png"
+    closeOptions();
+  }
+});
+
+const showOptions = () => {
+  arrowElement.setAttribute(
+    "src",
+    "https://img.icons8.com/ios/20/000000/delete-sign--v1.png"
+  );
+  optionsElement.classList.remove("closed");
+};
+
+const closeOptions = () => {
+  arrowElement.setAttribute(
+    "src",
+    "https://img.icons8.com/ios/20/000000/expand-arrow--v2.png"
+  );
+  optionsElement.classList.add("closed");
+};
+
+const checkIfOptionIsNotChosenAndIncludesText = (option, text) =>
+  !chosenOptions.includes(option) &&
+  option.toLowerCase().includes(text.toLowerCase());
+
+inputElement.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    showOptions();
+
+    const text = event.target.value;
+    const filteredOptions = gameOptions.filter((option) =>
+      checkIfOptionIsNotChosenAndIncludesText(option, text)
     );
-    optionsElement.classList.add("closed");
+
+    addOptionsToDropDown(filteredOptions);
   }
 });
