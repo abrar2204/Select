b47d2d9c20069cbe836bde1661d21b8630620968
Abrar Hasan <abrarhasan@Abrars-MacBook-Pro.local>
2022-04-10 14:10:19 +0530

    Add. selecting multiple options

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-const selectInput = document.getElementById("input");
-const selectArrow = document.getElementById("arrow");
-const selectOptions = document.getElementById("options");
+const inputElement = document.getElementById("input");
+const arrowElement = document.getElementById("arrow");
+const optionsElement = document.getElementById("options");
+const selectedOptionsElement = document.getElementById("selected-options");
 
 let chosenOptions = [];
-const gameOptions = ["Brachydios", "Rathalos", "Zinogre", "Safi''jiiva"];
+const gameOptions = ["Monster Hunter", "Witcher", "Uncharted", "Last of Us"];
+
+const addToSelectedOptions = (option) => {
+  const p = document.createElement("p");
+  p.append(option);
+  p.classList.add("option");
+
+  p.addEventListener("click", (e) => {
+    chosenOptions = chosenOptions.filter((o) => o !== option);
+    selectedOptionsElement.removeChild(p);
+  });
+
+  selectedOptionsElement.appendChild(p);
+};
 
 const addOptionsToDropDown = (options) => {
   const optionNodes = options.map((option) => {
     const p = document.createElement("p");
     p.append(option);
     p.classList.add("option");
-
+    p.addEventListener("click", () => {
+      addToSelectedOptions(option);
+      optionsElement.removeChild(p);
+      chosenOptions.push(option);
+    });
     return p;
   });
-  selectOptions.innerHTML = "";
-  optionNodes.forEach((o) => selectOptions.appendChild(o));
+  optionsElement.innerHTML = "";
+  optionNodes.forEach((o) => optionsElement.appendChild(o));
 };
 
 addOptionsToDropDown(gameOptions);
 
-selectArrow.addEventListener("click", (e) => {
-  if (selectOptions.classList.contains("closed")) {
-    selectArrow.setAttribute(
+arrowElement.addEventListener("click", (e) => {
+  if (optionsElement.classList.contains("closed")) {
+    arrowElement.setAttribute(
       "src",
       "https://img.icons8.com/ios/20/000000/delete-sign--v1.png"
     );
-    selectOptions.classList.remove("closed");
+    optionsElement.classList.remove("closed");
   } else {
-    selectArrow.setAttribute(
+    arrowElement.setAttribute(
       "src",
       "https://img.icons8.com/ios/20/000000/expand-arrow--v2.png"
     );
-    selectOptions.classList.add("closed");
+    optionsElement.classList.add("closed");
   }
 });
